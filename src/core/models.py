08e4a1e343fb6c874492348fba418a6b5225b951
faust <fanosport1@gmail.com>
2021-05-13 18:11:56 +0300

    add models

--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.views.generic import ListView
 
-# Create your models here.
+# from src.user.models import
+
+
+class Allstudents():
+    pass
+
+
+class Lecture(models.Model):
+    name_lecture = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    download = models.FileField(upload_to='media/')
+
+class Practical(models.Model, ListView):
+    name_lecture = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    download = models.FileField(upload_to='media/')
+    # take_student = models.Choices(choices=)
+
+
+
+
 
