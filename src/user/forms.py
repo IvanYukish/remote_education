661a0fb1dabb250493fe9df623353fde6e9d785d
IvanYukish <iwan.jukisch@gmail.com>
2021-05-17 16:34:08 +0300

    add type when we register user

--- src/user/forms.py
+++ src/user/forms.py
@@ -1,6 +1,8 @@
 from allauth.account.forms import SignupForm
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
+from .constants import USER_TYPE_CHOICES
 from .models import CustomUser
 
 
@@ -9,6 +11,13 @@ class CustomSignupForm(SignupForm):
                                  widget=forms.TextInput(attrs={'placeholder': _('Введіть своє ім\'я')}))
     last_name = forms.CharField(label=_('Прізвище'), max_length=150,
                                 widget=forms.TextInput(attrs={'placeholder': _('Введіть своє прізвище')}))
+    type = forms.ChoiceField(label=_('Тип'), choices=USER_TYPE_CHOICES)
+
+    def save(self, request):
+        user = super().save(request)
+        user.type = self.cleaned_data['type']
+        user.save()
+        return user
 
 
 class ProfileUpdateForm(forms.ModelForm):
